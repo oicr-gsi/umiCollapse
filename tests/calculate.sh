bc5017ce250cfcbd1b8685b6e84e3b08177f8ef0
Gavin Peng <gpeng@oicr.on.ca>
2024-03-13 12:10:10 -0400

    remove undeterminstic metrics

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -8,6 +8,5 @@ cd $1
 
 #find output files, return their md5sums to std out
 find . -name "*umiCountsPerPosition.tsv" -xtype f -exec sh -c "cat {} | md5sum " \;
-find . -name "*results.json" -xtype f -exec sh -c "cat {} | md5sum |sort" \;
-find . -name "*.bam" -xtype f -exec sh -c "cat {} | md5sum " \;
+find . -name "*preDedup.bamQC_results.json" -xtype f -exec sh -c "cat {} | md5sum |sort" \;
 ls | sed 's/.*\.//' | sort | uniq -c
